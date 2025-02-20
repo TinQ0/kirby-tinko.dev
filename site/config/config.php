47abc3aafa130b9bf4454105f2f474321f8f5ffd
Tinko Vialard <41490684+TinQ0@users.noreply.github.com>
2025-02-20 17:43:43 +0100

    fixed matomo id, should be number and not url of site

--- site/config/config.php
+++ site/config/config.php
@@ -8,6 +8,6 @@ return [
     'sylvainjule.footnotes.wrapper'  => 'aside',
 
     'sylvainjule.matomo.url'        => 'https://analytics.tinko.dev/matomo',
-    'sylvainjule.matomo.id'         => 'test.tinko.dev',
+    'sylvainjule.matomo.id'         => '1',
     'sylvainjule.matomo.token'      => 'ff5b30c6bea5bb6f224d6fa066d424e8',
 ];
