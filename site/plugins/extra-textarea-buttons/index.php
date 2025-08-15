ccff1e7444633b5667a42ee88fa48864127049af
Tinko Vialard <41490684+TinQ0@users.noreply.github.com>
2025-08-16 00:00:33 +0200

    Drop files from .gitignore

--- site/plugins/extra-textarea-buttons/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-Kirby::plugin('tinko/extra-textarea-buttons', [
-    'hooks' => [
-        'kirbytags:before' => function($text, $data, $options) {
-            // `==highlighted text==`
-            return preg_replace('/==(?=\S)(.+?)(?<=\S)==/', '<mark>$1</mark>', $text);
-        },
-    ],
-]);
--- site/plugins/footnotes/snippets/container.php
+++
@@ -1,5 +0,0 @@
-<<?php echo option('sylvainjule.footnotes.wrapper') ?> id="footnotes" class="footnotes-container">
-    <ol class="footnotes-list">
-        <?php echo $footnotes ?>
-    </ol>
-</<?php echo option('sylvainjule.footnotes.wrapper') ?>>
